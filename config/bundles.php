22e22d40a902745f97221dee4ddf5404c0b5bf7c
cheikh3008 <cheikh3008@gmail.com>
2021-09-22 22:25:54 +0000

    ok

--- config/bundles.php
+++ config/bundles.php
@@ -14,6 +14,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
 ];
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,47 +2,15 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Role;
-use App\Entity\User;
-use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
-
 {
-    private $encoder;
-    public function __construct(UserPasswordEncoderInterface $encoder)
-    {
-        $this->encoder = $encoder;
-    }
     public function load(ObjectManager $manager)
     {
-        $role = new Role;
-        $role->setLibelle('ROLE_ADMIN');
-        $manager->persist($role);
-
-        $role1 = new Role;
-        $role1->setLibelle('ROLE_TAILLEUR');
-        $manager->persist($role1);
-
-        $role2 = new Role;
-        $role2->setLibelle('ROLE_VISITEUR');
-        $manager->persist($role2);
-
-        $user = new User;
-        $user->setUsername('admin');
-        $user->setPrenom('bee');
-        $user->setNom('digital');
-        $user->setTelephone('773043248');
-        $user->setUsername('admin');
-        $user->setRole($role);
-        $user->setNomEntreprise('Bee Digital');
-        $user->setAdresse('Sacre Coeur 2');
-        $password = $this->encoder->encodePassword($user, 'admin123');
-        $user->setPassword($password);
-
-        $manager->persist($user);
+        // $product = new Product();
+        // $manager->persist($product);
 
         $manager->flush();
     }
